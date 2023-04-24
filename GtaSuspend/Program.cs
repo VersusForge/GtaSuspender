43c6a8cf30cb787c54f687fb86af2d3946e9c212
Vera@Versus <veraatversus@gmail.com>
2023-04-24 15:53:59 +0000

    edit readme

--- GtaSuspend/Program.cs
+++ GtaSuspend/Program.cs
@@ -33,34 +33,9 @@ namespace GtaSuspend {
       }
       HotKeyManager.RegisterHotKey(Keys.F8, KeyModifiers.Control, Suspend10Seconds);
       HotKeyManager.RegisterHotKey(Keys.F8, KeyModifiers.Alt, SuspendTrigger);
-      HotKeyManager.RegisterHotKey(Keys.F9, KeyModifiers.Control, JoinFriend);
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new SystemTrayContext());
-    }
-
-    private async static void JoinFriend(HotKeyEventArgs obj) {
-      if (!IsJoiningFriend) {
-        IsJoiningFriend = true;
-#if Errish
-        var steamid = Properties.Resources.SteamIdEndmann;
-#else
-        var steamid = Properties.Resources.SteamIdVera;
-#endif
-        //var steamid = "76561198004672963"; //Creedo
-        try {
-          var jsonstring = await HttpClient.GetStringAsync($"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={Properties.Resources.SteamApiKey}&format=json&steamids={steamid}");
-          var json = JavaScriptSerializer.Deserialize<dynamic>(jsonstring);
-          var h = json?["response"]?["players"];
-          if (json?["response"]?["players"]?[0]?["lobbysteamid"] is string lobbyId && json?["response"]?["players"]?[0]?["gameid"] is string gameId) {
-            Process.Start($"steam://joinlobby/{gameId}/{lobbyId}/{steamid}/");
-          }
-        }
-        catch (Exception e) {
-        }
-
-        IsJoiningFriend = false;
-      }
     }
 
     private static void SuspendTrigger(HotKeyEventArgs obj) {
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Gta 5 Suspender  
-start the suspender and he will running in the background. you can see a task tray icon.  
-Press Ctrl + F8 for suspend the game about 10 seconds, so you will be alone on the multiplayer map.  
-Press Alt + F8 to trigger suspend or resume manually for Gta5.  
+# GTA5 Suspender  
+
+- Download the executable from https://github.com/VersusForge/GtaSuspender/releases
+- Start the suspender and it will run in the background. You can see it running as a task tray icon.  
+- Any Time you press Press Ctrl + F8 one time, the game will be suspended for about 10 seconds and resume automatically.
+- So  just wait about 10 sec, and throw the lag you will be alone on the multiplayer map.  
+
+
+
+- Press Alt + F8 to toggle suspend or resume manually in Gta5. Typically not needed.
